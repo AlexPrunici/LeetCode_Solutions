af486dc6057624804da34d9ecba0ebc9f38c4113
AlexPrunici <prunici.alex@gmail.com>
2023-12-24 16:58:22 +0200

    valid_parentheses

--- Stack/validParentheses.py
+++ Stack/validParentheses.py
@@ -1,8 +1,22 @@
 class Solution:
     @staticmethod
     def is_valid(s: str) -> bool:
-        print(s)
-        return True
+        bracket_map = {
+            '[': ']',
+            '(': ')',
+            '{': '}'
+        }
+
+        stack = []
+
+        for c in s:
+            if c in bracket_map.keys():
+                stack.append(c)
+            elif c in bracket_map.values():
+                if len(stack) == 0 or bracket_map[stack.pop()] != c:
+                    return False
+
+        return len(stack) == 0
 
 
 if __name__ == '__main__':
